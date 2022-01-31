a3ed4e542e6bdf88ab4d414b1b32503d45b3dbbe
Wasif ur Rehman <wasifarbisoft@users.noreply.github.com>
2022-01-31 19:16:33 +0500

    Revert v4.1.14 changes (#4)
    * Revert v4.1.14 changes
    
    * Remove unused settings

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install
 
 # Constants #########################################################
 
-VERSION = '4.1.16'
+VERSION = '4.1.17'
 
 # Functions #########################################################
 
